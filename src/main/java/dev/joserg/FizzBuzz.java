acdcb2ba45de6f9cf5d1968469903254e11dc10e
joser <joserodriguezgarcia95@gmail.com>
2021-10-03 13:36:18 +0200

    Remove duplicated logic
    FizzBuzz returns FizzBuzz if given number is multiple of three and five
    
    FizzBuzz returns Buzz if given number is multiple of five
    
    FizzBuzz returns Fizz if given number is multiple of three
    
    FizzBuzz returns String of given number

--- src/main/java/dev/joserg/FizzBuzz.java
+++ src/main/java/dev/joserg/FizzBuzz.java
@@ -1,4 +1,22 @@
 package dev.joserg;
 
 public class FizzBuzz {
+    public static String of(Integer number) {
+        if (isFizzBuzz(number)) return "FizzBuzz";
+        if (isBuzz(number)) return "Buzz";
+        if (isFizz(number)) return "Fizz";
+        return number.toString();
+    }
+
+    private static boolean isFizzBuzz(Integer number) {
+        return isFizz(number) && isBuzz(number);
+    }
+
+    private static boolean isBuzz(Integer number) {
+        return number % 5 == 0;
+    }
+
+    private static boolean isFizz(Integer number) {
+        return number % 3 == 0;
+    }
 }
--- src/test/java/dev/joserg/FizzBuzzShould.java
+++ src/test/java/dev/joserg/FizzBuzzShould.java
@@ -1,4 +1,38 @@
 package dev.joserg;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class FizzBuzzShould {
+
+    @Test void
+    return_string_of_given_number() {
+        assertThat(FizzBuzz.of(1)).isEqualTo("1");
+        assertThat(FizzBuzz.of(2)).isEqualTo("2");
+        assertThat(FizzBuzz.of(4)).isEqualTo("4");
+    }
+
+    @Test void
+    return_Fizz_when_multiple_of_three() {
+        assertThat(FizzBuzz.of(3)).isEqualTo("Fizz");
+        assertThat(FizzBuzz.of(6)).isEqualTo("Fizz");
+        assertThat(FizzBuzz.of(9)).isEqualTo("Fizz");
+        assertThat(FizzBuzz.of(18)).isEqualTo("Fizz");
+    }
+
+    @Test void
+    return_Buzz_when_multiple_of_five() {
+        assertThat(FizzBuzz.of(5)).isEqualTo("Buzz");
+        assertThat(FizzBuzz.of(10)).isEqualTo("Buzz");
+        assertThat(FizzBuzz.of(20)).isEqualTo("Buzz");
+        assertThat(FizzBuzz.of(25)).isEqualTo("Buzz");
+    }
+
+    @Test void
+    return_FizzBuzz_when_multiple_of_three_and_five() {
+        assertThat(FizzBuzz.of(15)).isEqualTo("FizzBuzz");
+        assertThat(FizzBuzz.of(30)).isEqualTo("FizzBuzz");
+        assertThat(FizzBuzz.of(45)).isEqualTo("FizzBuzz");
+    }
 }
